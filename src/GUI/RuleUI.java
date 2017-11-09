08b5a946eb19fbcaebd7acadc1ae9ff478a0f1df
albocoder <albocoder@gmail.com>
2017-11-09 05:43:30 +0300

    can now add and remove rules. Rules is now a separate jpanel

--- src/GUI/RuleUI.java
+++ src/GUI/RuleUI.java
@@ -4,47 +4,66 @@ import cointracker.Rule;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
-public class RuleUI extends JPanel {            
+public class RuleUI extends JFrame {
+    // stuff in the panel
     private JButton ruleAdd,ruleDelete,ruleDisable;
-    private RulePanel rulePanel;
+    private ArrayList<RulePanel> rulePanels;
     private JPanel upperButtonsPanel,allRulesPanel;
     private JScrollPane allRulesPane;
-    private final ImageIcon trashimg,editimg,playimg,pauseimg;
+    // THE PANEL
+    private final JPanel fullWrapper;
+    private final DeleteHandler terminator;
     
     public RuleUI() {
-        trashimg = new ImageIcon(System.getProperty("user.dir")+"/img/trashbin.png");
-        editimg = new ImageIcon(System.getProperty("user.dir")+"/img/edit.png");
-        playimg = new ImageIcon(System.getProperty("user.dir")+"/img/play.png");
-        pauseimg = new ImageIcon(System.getProperty("user.dir")+"/img/pause.png");
-        rulePanel = new RulePanel();
+        fullWrapper = new JPanel();
+        terminator = new DeleteHandler();
         initComponents();
+        add(fullWrapper);
+        pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // temporary
     }
 
     @SuppressWarnings("unchecked")                    
     private void initComponents() {
         allRulesPane = new JScrollPane();
         allRulesPanel = new JPanel();
-        rulePanel = new RulePanel();
         upperButtonsPanel = new JPanel();
+        rulePanels = new ArrayList();
         ruleDelete = new JButton();
         ruleDisable = new JButton();
         ruleAdd = new JButton();
-        allRulesPanel.setPreferredSize(new Dimension(rulePanel.getWidth()+30,rulePanel.getHeight()*5));
-        allRulesPane.setPreferredSize(new Dimension(rulePanel.getWidth()+30,100));
-        allRulesPane.setViewportView(allRulesPanel);
-        RulePanel p2 = new RulePanel();
         
-        allRulesPanel.add(rulePanel);
-        allRulesPanel.add( new RulePanel());
-        allRulesPanel.add( new RulePanel());
-        allRulesPanel.add( new RulePanel());
-        allRulesPanel.add( new RulePanel());
-
+        allRulesPanel.setPreferredSize(new Dimension(400,500));//must be dynamic
+        allRulesPanel.add(new RulePanel(terminator,null));
+        allRulesPanel.add(new RulePanel(terminator,null));
+        allRulesPanel.add(new RulePanel(terminator,null));
+        allRulesPanel.add(new RulePanel(terminator,null));
+        
+        allRulesPane.setBorder(null); 
+        allRulesPane.setPreferredSize(new Dimension(440,250));
+        allRulesPane.getVerticalScrollBar().setUnitIncrement(10);
+        allRulesPane.setViewportView(allRulesPanel);
+        
+        upperButtonsPanel.setOpaque(false);
+        
+        ruleAdd.setText("Add Rule");
+        ruleAdd.setBackground(Color.GREEN);
+        ruleAdd.setFocusPainted(false);
+        ruleAdd.addActionListener(this::ruleDescriptionActionPerformed);
+        
         ruleDelete.setText("Delete Rule");
-
+        ruleDelete.setBackground(Color.RED);
+        ruleDelete.setFocusPainted(false);
+        
         ruleDisable.setText("Disable Rule");
-
+        ruleDisable.setBackground(Color.ORANGE);
+        ruleDisable.setFocusPainted(false);
+        
+        fullWrapper.setBackground(Color.BLUE);
+        fullWrapper.setPreferredSize(new Dimension(465,300));
+        
         GroupLayout upperButtonsPanelLayout = new GroupLayout(upperButtonsPanel);
         upperButtonsPanel.setLayout(upperButtonsPanelLayout);
         upperButtonsPanelLayout.setHorizontalGroup(
@@ -63,11 +82,9 @@ public class RuleUI extends JPanel {
                     .addComponent(ruleDisable))
                 .addGap(0, 0, Short.MAX_VALUE))
         );
-
-        ruleAdd.setText("Add Rule");
-
-        GroupLayout layout = new GroupLayout(this);
-        this.setLayout(layout);
+        
+        GroupLayout layout = new GroupLayout(fullWrapper);
+        fullWrapper.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -94,93 +111,27 @@ public class RuleUI extends JPanel {
     }
     
     public void showRule(Rule r){
-        allRulesPanel.add(new RulePanel(r));
+        allRulesPanel.add(new RulePanel(terminator,r));
+        revalidate();
     }
     private void ruleDescriptionActionPerformed(ActionEvent evt) {
-        
+        showRule(null);//somehow like this although it must be first not last.
     }
     
-    private class RulePanel extends JPanel{
-        private JCheckBox ruleCheckox;
-        private JButton deleteBtn,editBtn;
-        private JTextField ruleDescription;
-        private JToggleButton ruleToggleActivation;
-        private JPanel wrapper;
-
-        public RulePanel() {
-            wrapper = new JPanel();
-            ruleCheckox = new JCheckBox();
-            ruleDescription = new JTextField();
-            editBtn = new JButton();
-            deleteBtn = new JButton();
-            ruleToggleActivation = new JToggleButton();
-            editBtn.setBorder(null);
-            deleteBtn.setBorder(null);
-            editBtn.setBackground(Color.WHITE);
-            deleteBtn.setBackground(Color.WHITE);
-            editBtn.setIcon(editimg);
-            deleteBtn.setIcon(trashimg);
-            
-            ruleToggleActivation.setBorder(null);
-            ruleToggleActivation.setIcon(playimg);
-            ruleToggleActivation.setToolTipText("Disable the rule");
-            ruleCheckox.addActionListener(this::ruleCheckoxActionPerformed);
-            ruleDescription.addActionListener(this::ruleDescriptionActionPerformed);
-            ruleToggleActivation.addActionListener(this::ruleToggleActionPerformed);
-            
-            GroupLayout rulePanelLayout = new GroupLayout(wrapper);
-            wrapper.setLayout(rulePanelLayout);
-            rulePanelLayout.setHorizontalGroup(
-                rulePanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                .addGroup(rulePanelLayout.createSequentialGroup()
-                    .addComponent(ruleCheckox)
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                    .addComponent(ruleDescription, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                    .addComponent(ruleToggleActivation, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                    .addComponent(deleteBtn, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                    .addComponent(editBtn, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 0, Short.MAX_VALUE))
-            );
-            rulePanelLayout.setVerticalGroup(
-                rulePanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                .addGroup(rulePanelLayout.createSequentialGroup()
-                    .addContainerGap()
-                    .addGroup(rulePanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
-                        .addComponent(ruleCheckox)
-                        .addGroup(rulePanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                            .addComponent(ruleDescription, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
-                            .addComponent(ruleToggleActivation, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-                            .addComponent(deleteBtn, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-                            .addComponent(editBtn, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)))
-                    .addContainerGap(10, Short.MAX_VALUE))
-            );
-            add(wrapper);
+    public class DeleteHandler extends MouseAdapter{
+        @Override
+        public void mousePressed(MouseEvent e){
+            JButton b = (JButton)e.getComponent();
+            b.setIcon(RulePanel.closeimg_pressed);
         }
-
-        private RulePanel(Rule r) {
-            this();
-        }
-        
-        ///////////////////////// ACTION EVENT FUNCTIONS ///////////////////////
-        private void ruleToggleActionPerformed(ActionEvent evt) {                                            
-            if (ruleToggleActivation.isSelected()){
-                ruleToggleActivation.setIcon(pauseimg);
-                ruleToggleActivation.setToolTipText("Pause the rule");
-            }
-            else{
-                ruleToggleActivation.setIcon(playimg);
-                ruleToggleActivation.setToolTipText("Run the rule");
-            }
-        }
-        private void ruleCheckoxActionPerformed(ActionEvent evt) {                                           
-            // TODO add your handling code here:
-        }                                          
-
-        private void ruleDescriptionActionPerformed(ActionEvent evt) {                                            
-            // TODO add your handling code here:
+        @Override
+        public void mouseReleased(MouseEvent e){
+            JButton b = (JButton)e.getComponent();
+            b.setIcon(RulePanel.closeimg);
+            RulePanel rp = (RulePanel)(b.getParent().getParent());
+            rp.setVisible(false);
+            //find it in the arraylist and remove it from there 
+            //and stop it from running
         }
     }
 }
